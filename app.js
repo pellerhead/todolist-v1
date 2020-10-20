9d643f34784afbf9efe7fc515ed79874611240dd
Greg Nelson <nelsongreg35@gmail.com>
2020-10-19 21:46:59 -0500

    Starting to integrate database with app and render it

--- app.js
+++ app.js
@@ -1,36 +1,55 @@
 // jshint esversion:6
 const express = require("express");
 const bodyParser = require("body-parser");
-
+const mongoose = require("mongoose");
 const date = require(__dirname + "/date.js");
 
 const app = express();
-
-const items = ["Buy Food", "Cook Food", "Eat Food"];
-
-// You can still push to an array.  Does not have to be a let.
-const workItems = [];
 
 // For the embedded java script (EJS)
 app.set('view engine', 'ejs');
 
 // So we can pass data from page back to Server
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+
+mongoose.connect("mongodb://127.0.0.1:27017/todolistDB", {useNewUrlParser: true, useUnifiedTopology: true});
+
+const itemsSchema = {
+  name: String
+};
+
+const Item = mongoose.model("Item", itemsSchema);
+
+const item1 = new Item({
+  name: "Welcome to your todolist!"
+});
+
+const item2 = new Item({
+  name: "Hit the + buton to add a new item"
+});
+
+const item3 = new Item({
+  name: "<<-- Hit this to delete an item."
+});
+
+const defaultItems = [item1, item2, item3];
+
+Item.insertMany(defaultItems, function(err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Succesfully added default items to database.");
+  }
+});
 
 app.get("/", function(req, res) {
 
-  // Shwoing how to use different functions in a JavaScript Object/Class
   const day = date.getDate();
-  //let day = date.getDay();
 
-  res.render("list", {
-    listTitle: day,
-    newListItems: items
+  Item.find({}, function(err, foundItems){
+    res.render("list", {listTitle: day, newListItems: foundItems});
   });
-
 });
 
 app.post("/", function(req, res) {
